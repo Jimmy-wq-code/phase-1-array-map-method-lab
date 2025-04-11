0472e19c2d22593dcbc6f5e0e6c32e867d86d799
Jimmy Mike Okwiri <jimmy.okwiri@student.moringaschool.com>
2025-04-11 04:21:41 +0300

    Modifies index.js

--- index.js
+++ index.js
@@ -9,8 +9,18 @@ const tutorials = [
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
+  
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {
+    return tutorial
+      .split(' ')
+      .map(word => word[0].toUpperCase() + word.slice(1))
+      .join(' ');
+  });
+};
+
+module.exports = {
+  titleCased
+};
